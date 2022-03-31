79aad2ed810f93aba2dfe2d14ec92baafec3fd75
Sonins <gmlrhks95@gmail.com>
2022-03-31 17:46:19 +0900

    Added calc_anchors_3 node.

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -80,6 +80,27 @@ def yolo_pipeline():
         .apply(onprem.mount_pvc("yolo-data-pvc", "yolo-data", "/data"))
     ).after(load_data_1)
 
+    calc_anchors_3 = (
+        dsl.ContainerOp(
+            name="Calculate anchors from data.",
+            image="daisukekobayashi/darknet:cpu-noopt",
+            arguments=[
+                "sh",
+                "-c",
+                "darknet detector calc_anchors $DATA_PATH -num_of_clusters $CLUSTER_NUM -width $WIDTH -height $HEIGHT -dont_show && mv anchors.txt /data",
+            ],
+        )
+        .set_display_name("Calculate anchors")
+        .apply(onprem.mount_pvc("yolo-data-pvc", "yolo-data", "/data"))
+    ).after(test_train_split_2)
+
+    calc_anchors_3.container.add_env_variable(
+        V1EnvVar(name="DATA_PATH", value="/data/obj.data")
+    ).add_env_variable(V1EnvVar(name="CLUSTER_NUM", value=9))\
+    .add_env_variable(V1EnvVar(name="WIDTH", value=416))\
+    .add_env_variable(V1EnvVar(name="HEIGHT", value=416))\
+    .add_env_variable(V1EnvVar(name="OUTPUT_PATH", value="/data/anchors.txt"))
+
 
 if __name__ == "__main__":
 
